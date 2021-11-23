b88ab6156bbbb874871bffd3dd356f588593e538
VaibhavTekkalur <tekkalurvaibhav@gmail.com>
2021-11-23 21:21:50 +0530

    mobile media query added and console logs removed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const html = document.querySelector('html');
 
 const volumeslider = document.querySelector('#volumeslider');
 let volume = 0.2;
-console.log(volume);
+
 
 const playsound = function(id){
     link = `./sounds/${id}.wav`;
@@ -15,7 +15,6 @@ const playsound = function(id){
 
 drumkit.addEventListener('click',e =>{
     const id = e.target.id;
-    console.log(id);
     if (e.target.id != ''){
         playsound(id);
     }
@@ -23,7 +22,6 @@ drumkit.addEventListener('click',e =>{
 
 volumeslider.addEventListener('input', e=> {
     volume = e.target.value;
-    console.log(volume);
 });
 
 html.addEventListener('keydown' , e => {
