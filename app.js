3afacc7ea70380fadae6a4cedae29fc9dbac54bd
VaibhavTekkalur <tekkalurvaibhav@gmail.com>
2021-11-23 17:09:25 +0530

    Layouts , Volume bar , Final Styling

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const drumkit = document.querySelector('.drumkit');
 const html = document.querySelector('html');
 
+const volumeslider = document.querySelector('#volumeslider');
+let volume = 0.2;
+console.log(volume);
+
 const playsound = function(id){
     link = `./sounds/${id}.wav`;
     const audio = document.createElement('audio');
     audio.src = link;
-    audio.volume = 0.2;
+    audio.volume = volume;
     audio.play();
 };
 
 drumkit.addEventListener('click',e =>{
     const id = e.target.id;
+    console.log(id);
     if (e.target.id != ''){
         playsound(id);
     }
 });
 
+volumeslider.addEventListener('input', e=> {
+    volume = e.target.value;
+    console.log(volume);
+});
